f199611e9788589e2dbd542bc10fa194006f3e6a
landon.liles@sbcglobal.net <landon.liles@sbcglobal.net>
2023-09-11 17:37:53 -0500

    add sum function

--- hw3.cpp
+++ hw3.cpp
@@ -1,2 +1,24 @@
+#include <vector>
 #include <iostream>
 using namespace std;
+
+int sumList(vector<int> list){
+    int sum = 0;
+    for(int i = 0;i < list.size();i++){
+        sum += list[i];
+    }
+
+    return sum;
+}
+
+int main(){
+    vector<int> ls;
+    ls.push_back(1);
+    ls.push_back(2);
+    ls.push_back(2);
+
+    int s = sumList(ls);
+    cout << s;
+    return 0;
+}
+
