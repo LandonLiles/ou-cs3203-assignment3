c2fa175a56c8181451baf5df125e9c86600fd868
landon.liles@sbcglobal.net <landon.liles@sbcglobal.net>
2023-09-13 21:07:01 -0500

    number 10

--- hw3.cpp
+++ hw3.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 using namespace std;
 
+// Returns sum of list numbers
 int sumList(vector<int> list){
     int sum = 0;
     for(int i = 0;i < list.size();i++){
@@ -11,6 +12,7 @@ int sumList(vector<int> list){
     return sum;
 }
 
+// Returns product of list numbers
 int prodList(vector<int> list){
     int product = list[0];
     for(int i = 1;i < list.size();i++){
@@ -20,10 +22,12 @@ int prodList(vector<int> list){
     return product;
 }
 
+// Main method
 int main(){
     vector<int> ls;
     int temp = 0;
     
+    // Loop that inputs numbers into list, enter -1 to stop 
     while(temp != -1){
         cout << "enter a number, enter -1 to stop:";
         cin >> temp;
