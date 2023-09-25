e0e1668efe4acccf63a61a59ef1b7cd7b684bb5c
landon.liles@sbcglobal.net <landon.liles@sbcglobal.net>
2023-09-25 16:59:00 -0500

    p10

--- hw3.cpp
+++ hw3.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-// Returns sum of list numbers
+// Function that returns the sum of a list's numbers
 int sumList(vector<int> list){
     int sum = 0;
     for(int i = 0;i < list.size();i++){
@@ -12,7 +12,7 @@ int sumList(vector<int> list){
     return sum;
 }
 
-// Returns product of list numbers
+// Function that returns the product of a list's numbers
 int prodList(vector<int> list){
     int product = list[0];
     for(int i = 1;i < list.size();i++){
@@ -34,13 +34,13 @@ vector<int> revList(vector<int> list){
     return fin;
 }
 
-// Main method
+// Main method .
 int main(){
     vector<int> ls;
     int temp = 0;
     int ct = 0;
-    
-    // Loop that inputs numbers into list, enter -1 to stop 
+
+    // Loop that receives list input from the user until '-1' is entered
     while(temp != -1){
         cout << "enter a number, enter -1 to stop:";
         cin >> temp;
